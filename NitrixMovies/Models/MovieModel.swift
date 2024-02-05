9b1a5c4c2aeabcc6055e80cd6d9564deacf8ad28
SashkaToropov <toropov.oleksandr03@gmail.com>
2024-02-05 18:02:15 +0200

    Removed extra code in Movie model

--- NitrixMovies/Models/MovieModel.swift
+++ NitrixMovies/Models/MovieModel.swift
@@ -13,29 +13,21 @@ struct MovieModel: Codable {
 
 // MARK: - Result
 struct Movie: Codable {
-    let adult: Bool
     let backdropPath: String
     let genreIDS: [Int]
     let id: Int
-    let originalLanguage, originalTitle, overview: String
-    let popularity: Double
+    let overview: String
     let posterPath, releaseDate, title: String
-    let video: Bool
     let voteAverage: Double
-    let voteCount: Int
 
     enum CodingKeys: String, CodingKey {
-        case adult
         case backdropPath = "backdrop_path"
         case genreIDS = "genre_ids"
         case id
-        case originalLanguage = "original_language"
-        case originalTitle = "original_title"
-        case overview, popularity
+        case overview
         case posterPath = "poster_path"
         case releaseDate = "release_date"
-        case title, video
+        case title
         case voteAverage = "vote_average"
-        case voteCount = "vote_count"
     }
 }
